b0559767a98e62602f2faa993383619eaef16642
taanguyen <taa.nguyen@gmail.com>
2020-06-13 01:01:10 -0400

    write user quote to quotes.db, needs query and unique id

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require('express');
+const Datastore = require('nedb');
+const { response } = require('express');
+
 const app = express();
+const db = new Datastore('quotes.db');
+db.loadDatabase();
+
 app.listen(3000, () => console.log("you are listening to port 3000"));
 app.use(express.static('public'));
 app.use(express.text());
-// connect to a database
-// handle http requests 
+
 // users input a quote, hit submit, update db
 app.post("/", function(req, res){
     // update db 
-
-    console.log("i have a request!");
-    console.log(req.body);
+    db.insert({quote: req.body});
     // query db for a quote to send back
-    const quoteResponse = "Thank you for your quote";
-    res.send(quoteResponse);
-    
+    db.find({}, (err, data) => {
+        if(err) throw err;
+        res.send(data);
+    });
 });
 
