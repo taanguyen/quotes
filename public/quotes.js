3185f36176bd9cb61547c38121764160d92d0c09
taanguyen <taa.nguyen@gmail.com>
2020-06-15 02:30:35 -0400

    remove comments

--- public/quotes.js
+++ public/quotes.js
@@ -1,8 +1,6 @@
 // user input is black
 const userQuoteElem = document.getElementById("user-quote");
-//const userQuote = document.createTextNode("");
 userQuoteElem.addEventListener("input", e =>{
-    //console.log("you entered: " + e.target.value);
     userQuoteElem.style.color = "black";
 });
 // scroll when user hits submit
